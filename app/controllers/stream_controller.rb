05feec63d47e35e3b96565de88095c0dfcd87911
Javier Vegas <javiervegas@Javier-Vegass-MacBook-Pro.local>
2011-02-17 23:28:07 -0800

    better extract youtube video id

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -2,17 +2,15 @@ class StreamController < ApplicationController
   def index
     # This will pull a sample of all tweets based on
     # your Twitter account's Streaming API role.
-    client = TweetStream::Client.new('javiervegas','9yogurines9')
-    client.on_limit do |skip_count|
-      return "limit #{skip_count}"
-    end
-    client.track('youtube') do |status|
+    TweetStream::Client.new('javiervegas','9yogurines9').track('youtube') do |@status|
       # The status object is a special Hash with
       # method access to its keys.
-      @text =  status.text
-      @url = status.entities.urls.first.andand["url"]
-      @id = @text.scan(/youtu\.be\/([-\w]+)/).first
-      puts "#{@text}  => #{@url}"
+      yturl = @status.entities.urls.first.andand["expanded_url"] || @status.entities.urls.first.andand["url"]
+      @id = yturl.scan(/youtube.com\/watch\?v=([-\w]+)/).first || yturl.scan(/youtu\.be\/([-\w]+)/).first
+      puts @status.text
+      puts @status.entities.urls.first.andand["url"]
+      puts @status.entities.urls.first.andand["expanded_url"]
+      puts @id
       break if !@id.nil?
     end
   end
