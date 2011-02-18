dd92919fc208f1d86a85d6ea48bddf0572be6777
Javier Vegas <javiervegas@Javier-Vegass-MacBook-Pro.local>
2011-02-18 00:44:15 -0800

    support for vimeo videos

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -1,12 +1,12 @@
 class StreamController < ApplicationController
   def index
     # This will pull a sample of all tweets based on
-    # your Twitter account's Streaming API role.
-    TweetStream::Client.new('javiervegas','9yogurines9').track('youtube') do |@status|
+    # our Twitter account's Streaming API role.
+    TweetStream::Client.new('javiervegas','9yogurines9').track('vimeo','youtube') do |@status|
       # The status object is a special Hash with
       # method access to its keys.
       @yturl = @status.entities.urls.first.andand["expanded_url"] || @status.entities.urls.first.andand["url"]
-      @id = @yturl.scan(/youtube.com\/watch\?v=([-\w]+)/).first || @yturl.scan(/youtu\.be\/([-\w]+)/).first
+      @id = @yturl.scan(/youtube.com\/watch\?v=([-\w]+)/).first || @yturl.scan(/youtu\.be\/([-\w]+)/).first || @yturl.scan(/vimeo.com\/\D*(\d+)/).first
       puts @status.text
       puts @status.entities.urls.first.andand["url"]
       puts @status.entities.urls.first.andand["expanded_url"]
