706d2b1c6b9c5231181d587c6ff5b9e0116b787b
Javier Vegas <javiervegas@Javier-Vegass-MacBook-Pro.local>
2011-02-18 23:30:28 -0800

    rename variable

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -5,16 +5,16 @@ class StreamController < ApplicationController
     TweetStream::Client.new('javiervegas','9yogurines9').track('vimeo','youtube') do |@status|
       # The status object is a special Hash with
       # method access to its keys.
-      @yturl = @status.entities.urls.first.andand["expanded_url"] || @status.entities.urls.first.andand["url"]
-      @id = @yturl.scan(/youtube.com\/watch\?v=([-\w]+)/).first || @yturl.scan(/youtu\.be\/([-\w]+)/).first || @yturl.scan(/vimeo.com\/\D*(\d+)/).first
+      @videorurl = @status.entities.urls.first.andand["expanded_url"] || @status.entities.urls.first.andand["url"]
+      @id = @videorurl.scan(/youtube.com\/watch\?v=([-\w]+)/).first #|| @videorurl.scan(/youtu\.be\/([-\w]+)/).first || @videorurl.scan(/vimeo.com\/\D*(\d+)/).first
       puts @status.text
       puts @status.entities.urls.first.andand["url"]
       puts @status.entities.urls.first.andand["expanded_url"]
-      puts @yturl
+      puts @videorurl
       puts @id
       break if !@id.nil?
     end
-    @video = VideoInfo.new(@yturl)
+    @video = VideoInfo.new(@videorurl)
   end
 
   def show
