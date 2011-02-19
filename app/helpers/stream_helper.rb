1c0a1373503db6b4c3830c9cb43ee3a27ede3615
Javier Vegas <javiervegas@Javier-Vegass-MacBook-Pro.local>
2011-02-19 14:27:46 -0800

    helper function to display seconds as minutes plus seconds

--- app/helpers/stream_helper.rb
+++ app/helpers/stream_helper.rb
@@ -1,2 +1,7 @@
 module StreamHelper
+  def convert_seconds_to_time(seconds)
+    full_minutes = seconds / 1.minutes
+    seconds_in_last_minute = seconds - full_minutes.minutes.seconds
+    (full_minutes>0?"#{full_minutes} minutes ":"")+"#{seconds_in_last_minute} seconds"
+  end
 end
