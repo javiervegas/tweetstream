b0ebaa62cb8681a4dd567f2cdaf860bbfd52fb2b
Javier Vegas <javiervegas@Javier-Vegass-MacBook-Pro.local>
2011-02-22 00:30:00 -0800

    move logic to queue worker

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -2,20 +2,11 @@ class StreamController < ApplicationController
   def index
     # This will pull a sample of all tweets based on
     # our Twitter account's Streaming API role.
-    until !@id.nil?
-      redis = Redis.new
-      @status = TweetStream::Status.new(JSON.parse(redis.get("tweet")))
-      # The status object is a special Hash with
-      # method access to its keys.
-      @videorurl = @status.entities.urls.first.andand["expanded_url"] || @status.entities.urls.first.andand["url"]
-      @id = @videorurl.scan(/youtube.com\/watch\?v=([-\w]+)/).first #|| @videorurl.scan(/youtu\.be\/([-\w]+)/).first || @videorurl.scan(/vimeo.com\/\D*(\d+)/).first
-      puts @status.text
-      puts @status.entities.urls.first.andand["url"]
-      puts @status.entities.urls.first.andand["expanded_url"]
-      puts @videorurl
-      puts @id
-    end
-    @video = VideoInfo.new(@videorurl)
+    redis = Redis.new
+    @status = TweetStream::Status.new(JSON.parse(redis.get("tweet")))
+    # The status object is a special Hash with
+    # method access to its keys.
+    @video = VideoInfo.new(@status.videorurl)
   end
 
   def show
