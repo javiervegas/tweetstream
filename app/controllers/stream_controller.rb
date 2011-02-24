4ec250bc3ce3ae7a5c7f741234d0cbe0523fb155
Javier Vegas <javiervegas@Javier-Vegass-MacBook-Pro.local>
2011-02-23 23:25:40 -0800

    save viewed videos

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -4,6 +4,7 @@ class StreamController < ApplicationController
     # our Twitter account's Streaming API role.
     redis = Redis::Namespace.new(:tweetstream,:redis => Redis.new)
     @status = TweetStream::Status.new(JSON.parse(redis.get("tweet")))
+    redis.setnx(@status.id,@status)
     # The status object is a special Hash with
     # method access to its keys.
     @video = @status.video.info.video
